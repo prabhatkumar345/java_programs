84ab35633e8b1e77de02751a0d9294d7b5fbfb7f
Prabhat Kumar Pendro <prabhatpendro654321@gmail.com>
2026-02-24 00:17:31 +0530

    Initial Commit

---
+++ java_program/java33.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class java33{
+    public static int factorial(int num){
+        if(num==0){
+            return 1;
+        }
+        if(num==1){
+            return 1;
+        }
+        int fact=1;
+        fact=factorial(num-1);
+        fact=fact*num;
+        return fact;
+
+    }
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number whose factorial you want:");
+        int num=sc.nextInt();
+        System.out.println("Factorial:"+factorial(num));
+
+
+    }
+}
