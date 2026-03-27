6bf76012ae73d6e01c218d751041d883134397d1
Prabhat Kumar Pendro <prabhatpendro654321@gmail.com>
2026-03-27 22:00:19 +0530

    Initial Commit

---
+++ java_program/MillisecondToMiniuteAndSecond.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MillisecondToMiniuteAndSecond {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the second in millisecond:");
+        int ms=sc.nextInt();
+
+        double totalsecond=ms/1000;
+        System.out.println("Miilisecond to minitus:"+totalsecond/60);
+        System.out.println("Millisecond to seconds:"+totalsecond);
+
+    }
+    
+}
